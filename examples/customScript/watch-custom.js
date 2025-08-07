080d7421fa88e719d08e064255f56adb52c58d2c
ccg <222>
2025-08-07 13:16:46 +0800

    实时更新custom

--- examples/customScript/watch-custom.js
+++ examples/customScript/watch-custom.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import chokidar from "chokidar";
-import { updateCustomExamples } from "../../examples/customScript/update-custom-examples.js";
+import { updateCustomExamples } from "./update-custom-examples.js";
 
 console.log("开始监听 examples/custom 目录的变化...");
 
@@ -13,7 +13,7 @@ try {
 }
 
 // 监听 examples/custom 目录的变化
-const watcher = chokidar.watch("./examples/custom", {
+const watcher = chokidar.watch("../custom", {
   ignored: /(^|[\/\\])\../, // 忽略隐藏文件
   persistent: true,
   ignoreInitial: false, // 初始化时也触发一次
